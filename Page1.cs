f88a912182b0fdb46ddcbdfb1058ac26a03b7b57
Gabriele <Gabriele@GDG1>
2023-11-29 19:38:03 +0100

    Aggiungere i file di progetto.

---
+++ Page1.cs
@@ -0,0 +1,12 @@
+﻿using Wisej.Web;
+
+namespace TESTGITHUB
+{
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+    }
+}
